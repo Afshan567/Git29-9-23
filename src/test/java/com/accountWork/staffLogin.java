4dc38bb90681cfefc658d4eaa9dca12342cf1aef
Afshan567 <afshanjabbar368@gmail.com>
2023-09-29 15:50:23 +0530

    commit 2

---
+++ src/test/java/com/accountWork/staffLogin.java
@@ -0,0 +1,11 @@
+package com.accountWork;
+
+import org.testng.annotations.Test;
+
+public class staffLogin {
+@Test
+public void staffLgn()
+{
+	System.out.println("staff login test script");
+}
+}
